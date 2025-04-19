2601cb946b96c0b301e4f2aae16ea55197944188
divyashreej <42132028+divyashreej@users.noreply.github.com>
2025-04-19 12:16:30 +0530

    test the 3 run command modes- local,ssh and docker (#12)
    * test the 3 run command modes- local,ssh and docker
    * Add emptylines at the end of file
    
    Signed-off-by: Divyashree Jayaram <divyashreej15@gmail.com>

--- binnacle/plugins/commands.py
+++ binnacle/plugins/commands.py
@@ -22,7 +22,8 @@ def full_cmd(cmd):
         return f"""ssh {_command_config.ssh_user}@{_command_config.node} \
         -i {_command_config.ssh_pem_file} -p {_command_config.ssh_port}  \
         '{escaped_ssh_cmd(cmd)}'
-        """
+        """ 
+
     elif _command_config.mode == 'docker':
         return f"""docker exec -i {_command_config.node} /bin/bash -c '{escaped_cmd(cmd)}'"""
 
@@ -31,6 +32,7 @@ def full_cmd(cmd):
 
 @binnacle_task
 def command_run(cmd, ret = 0, task=None):
+    cmd = full_cmd(cmd)
     result = subprocess.run(cmd, shell=True, capture_output=True, universal_newlines=True)
     task.ok = result.returncode == ret
     if not task.ok:
@@ -44,6 +46,7 @@ class CommandConfig:
         self.mode = ""
         self.ssh_user = "root"
         self.ssh_pem_file = ""
+        self.ssh_port = ""
 
 
 _command_config = CommandConfig()
@@ -60,3 +63,16 @@ def command_container(name):
 def command_mode(name):
     _command_config.mode = name
 
+
+def command_ssh_user(name):
+    _command_config.ssh_user = name
+    
+def command_ssh_sudo(name):
+    _command_config.sudo = name
+
+def command_ssh_pem_file(name):
+    _command_config.ssh_pem_file = name
+
+def command_port(name):
+    _command_config.ssh_port = name
+    
---
+++ test_commands_local.py
@@ -0,0 +1,7 @@
+from binnacle import *
+
+#mode1: local
+validated_json('{"name": 100}')
+command_run("ls /tmp")
+command_run("stat /home/ubuntu/reports.pdf")
+
